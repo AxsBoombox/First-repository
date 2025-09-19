381b596f4c9b43309c6d8d9b0093ef2d4a8dd4de
AxsBoombox <austinscutt@woldgate.net>
2025-09-19 14:58:26 +0100

    basic test
    used to test the functions in the history tab

--- thirdtryatgitthing/Program.cs
+++ thirdtryatgitthing/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("How are you?");
+string input = Console.ReadLine();
+Console.WriteLine("ok");
